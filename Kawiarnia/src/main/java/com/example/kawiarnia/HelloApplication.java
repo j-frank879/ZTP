7c28fff1bcbe400b014275610b9852e36eeebf42
Joanna Frankowska <j.frankowska8844@gmail.com>
2022-12-12 21:16:34 +0100

    init project

---
+++ Kawiarnia/src/main/java/com/example/kawiarnia/HelloApplication.java
@@ -0,0 +1,24 @@
+package com.example.kawiarnia;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("menu.fxml"));
+       // FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 640 , 480);
+        stage.setTitle("Kawiarnia");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
